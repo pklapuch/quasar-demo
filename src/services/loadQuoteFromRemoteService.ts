7b26e720a9aa035a46d9fd5372df2101a2d4daf2
Pawel Klapuch <pawelklapuch@gmail.com>
2024-10-07 16:38:16 +0200

    Clean Up

--- src/services/loadQuoteFromRemoteService.ts
+++ src/services/loadQuoteFromRemoteService.ts
@@ -1,18 +1,29 @@
-import { AxiosResponse } from 'axios';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { api } from 'boot/axios';
+
+type Headers = {
+  [key: string]: string;
+};
 
 // Returns: String
 // Throws: any Error
 export const loadQuoteFromRemoteService = async function () {
   try {
     const response = await api.get('/v1/quotes');
-    return mapResponse(response.data, response.status, response.headers);
+
+    const headers: Headers = {};
+    for (const key in response.headers) {
+      const value = response.headers[key];
+      headers[key] = value;
+    }
+
+    return mapResponse(response.data, response.status, headers);
   } catch (error) {
     throw error;
   }
 };
 
-function mapResponse(data, status, headers) {
+function mapResponse(data: [string: any], status: number, headers: Headers) {
   console.log(status);
   console.log(data);
   console.log(headers);
